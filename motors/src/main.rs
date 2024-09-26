74ad827b4ee80af5c71a32e7c81413f4c64833d0
Benjamin Bolte <ben@bolte.cc>
2024-09-26 22:20:11 +0800

    rust version

--- motors/src/main.rs
+++ motors/src/main.rs
@@ -11,39 +11,7 @@ use tokio::time;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut sock_rx = CanSocket::open("can0")?;
-    let sock_tx = CanSocket::open("can0")?;
-
-    let mut rs_util = RobStrideUtils::new(0x7f);
-
-    sock_tx.write_frame(rs_util.request_dev_id())?.await?;
-    sock_tx.write_frame(rs_util.request_enable())?.await?;
-    sock_tx
-        .write_frame(rs_util.request_motion(0.0, 1f32, 0f32, 0.2f32, 0.1f32))?
-        .await?;
-    // sock_tx.write_frame(rs_util.request_stop(StopMode::Normal))?.await?;
-
-    tokio::spawn(async move {
-        sock_tx
-            .write_frame(rs_util.request_param(0x302d))
-            .expect("Error in reading param");
-        time::sleep(Duration::from_millis(1)).await;
-    });
-
-    while let Some(res) = sock_rx.next().await {
-        match res {
-            Ok(CanFrame::Data(frame)) => unsafe {
-                if frame.data().len() == 8 {
-                    let fdata = &frame.data()[4..8].align_to::<f32>();
-                    println!("{:?}", fdata);
-                }
-                println!("{:?}", &frame);
-            },
-            Ok(CanFrame::Remote(frame)) => println!("{:?}", frame),
-            Ok(CanFrame::Error(frame)) => println!("{:?}", frame),
-            Err(err) => eprintln!("{}", err),
-        }
-    }
+    robstride::debug_main();
 
     Ok(())
 }
--- motors/src/robstride.rs
+++ motors/src/robstride.rs
@@ -1,102 +1,216 @@
-use socketcan::{CanFrame, EmbeddedFrame, ExtendedId};
-use std::f32::consts::PI;
-use std::ptr::addr_of_mut;
+use serialport::SerialPort;
+use std::io::Write;
+use std::time::Duration;
 
-pub enum StopMode {
-    Normal = 0,
-    ClearErr = 1,
+const MAX_PORTS: usize = 10;
+
+const CAN_ID_MASTER: u8 = 0x00; // Control host address - SPIE
+const CAN_ID_MOTOR_DEFAULT: u8 = 0x7F; // Default motor address - Unconfigured ID
+const CAN_ID_BROADCAST: u8 = 0xFE; // Broadcast address - Default receive address
+const CAN_ID_DEBUG_UI: u8 = 0xFD; // Debug address - Upper computer address
+
+// Ubuntu
+const BAUDRATE: u32 = 921600;
+const TTY_PORT: &str = "/dev/ttyCH341USB0";
+
+// Mac
+// Note that this baudrate will let you send something to the device on Mac, but
+// it won't work because the device baudrate is supposed to be 921600.
+// const BAUDRATE: u32 = 115200;
+// const TTY_PORT: &str = "/dev/tty.usbserial-110";
+
+#[repr(u8)]
+enum CanComMode {
+    AnnounceDevId = 0,
+    MotorCtrl,
+    MotorFeedback,
+    MotorIn,
+    MotorReset,
+    MotorCali,
+    MotorZero,
+    MotorId,
+    ParaWrite,
+    ParaRead,
+    ParaUpdate,
+    OtaStart,
+    OtaInfo,
+    OtaIng,
+    OtaEnd,
+    CaliIng,
+    CaliRst,
+    SdoRead,
+    SdoWrite,
+    ParaStrInfo,
+    MotorBrake,
+    FaultWarn,
+    ModeTotal,
 }
 
-pub struct RobStrideUtils {
-    id: u8,
+#[repr(u8)]
+enum MotorMode {
+    Reset = 0,
+    Cali,
+    Motor,
+    Brake,
 }
 
-impl RobStrideUtils {
-    pub fn new(id: u8) -> Self {
-        Self { id }
+struct MotorStatus {
+    under_volt_fault: bool,
+    over_cur_fault: bool,
+    over_temp_fault: bool,
+    encoder_fault: bool,
+    hall_fault: bool,
+    no_cali_fault: bool,
+    mode: MotorMode,
+}
+
+struct ExId(u32);
+
+impl ExId {
+    fn new(id: u8, data: u16, mode: u8, res: u8) -> ExId {
+        let mut value: u32 = 0;
+        value |= (res as u32 & 0x07) << 29;
+        value |= (mode as u32 & 0x1F) << 24;
+        value |= (data as u32 & 0xFFFF) << 8;
+        value |= id as u32 & 0xFF;
+        ExId(value)
     }
 
-    fn set_extended_id(&mut self, com_type: u8, data_field: u16, destination: u8) -> ExtendedId {
-        ExtendedId::new((com_type as u32) << 24 | (data_field as u32) << 8 | (destination as u32))
-            .unwrap()
+    fn to_addr(&self) -> u32 {
+        let exid_u32 = self.0;
+        (exid_u32 << 3) | 0x00000004
+    }
+}
+
+#[derive(Default)]
+struct CanPack {
+    ex_id: ExId,
+    len: u8,
+    data: [u8; 8],
+}
+
+fn txd_pack(serial_port: &mut dyn SerialPort, pack: &CanPack) {
+    println!("Sending data");
+
+    let mut len = pack.len;
+    let mut data = pack.data;
+
+    if len < 1 {
+        len = 1;
+        data[0] = 0;
+    } else if len > 8 {
+        len = 8;
     }
 
-    pub fn request_dev_id(&mut self) -> CanFrame {
-        CanFrame::new(self.set_extended_id(0, 0x0000, self.id), &[0]).unwrap()
+    let addr = pack.ex_id.to_addr();
+
+    let mut packet = Vec::with_capacity(9 + len as usize);
+    packet.push(b'A');
+    packet.push(b'T');
+
+    packet.push(((addr & 0xFF000000) >> 24) as u8);
+    packet.push(((addr & 0x00FF0000) >> 16) as u8);
+    packet.push(((addr & 0x0000FF00) >> 8) as u8);
+    packet.push((addr & 0x000000FF) as u8);
+
+    packet.push(len);
+
+    for i in 0..len as usize {
+        packet.push(data[i]);
     }
 
-    pub fn request_motion(
-        &mut self,
-        torque: f32,
-        angle: f32,
-        velocity: f32,
-        kp: f32,
-        kd: f32,
-    ) -> CanFrame {
-        let torqueu16 = ((torque + 17f32 / 34f32) * 65535f32) as u16;
-        let angleu16 = ((angle + 4f32 * PI / 8f32 * PI) * 65535f32) as u16;
-        let velocityu16 = ((velocity + 44f32 / 88f32) * 65535f32) as u16;
-        let kpu16 = ((kp / 500f32) * 65535f32) as u16;
-        let kdu16 = ((kd / 5f32) * 65535f32) as u16;
+    packet.push(b'\r');
+    packet.push(b'\n');
 
-        CanFrame::new(
-            self.set_extended_id(1, torqueu16, self.id),
-            &[
-                (angleu16 >> 8) as u8,
-                angleu16 as u8,
-                (velocityu16 >> 8) as u8,
-                velocityu16 as u8,
-                (kpu16 >> 8) as u8,
-                kpu16 as u8,
-                (kdu16 >> 8) as u8,
-                kdu16 as u8,
-            ],
-        )
-        .unwrap()
+    // Print the full packet
+    print!("Full packet (hex): ");
+    for byte in &packet {
+        print!("{:02X} ", byte);
+    }
+    println!();
+
+    // Write the data
+    match serial_port.write(&packet) {
+        Ok(bytes_written) => {
+            if bytes_written != packet.len() {
+                eprintln!(
+                    "Warning: Not all bytes written. Expected {}, wrote {}",
+                    packet.len(),
+                    bytes_written
+                );
+            } else {
+                println!("Data sent successfully");
+            }
+        }
+        Err(e) => {
+            eprintln!("Error writing to serial port: {}", e);
+        }
     }
 
-    pub fn request_enable(&mut self) -> CanFrame {
-        CanFrame::new(self.set_extended_id(3, 0x0000, self.id), &[0]).unwrap()
+    // Flush the output buffer
+    if let Err(e) = serial_port.flush() {
+        eprintln!("Error flushing serial port: {}", e);
     }
+}
 
-    pub fn request_stop(&mut self, mode: StopMode) -> CanFrame {
-        CanFrame::new(self.set_extended_id(4, 0x0000, self.id), &[mode as u8]).unwrap()
-    }
+fn init_serial_port(device: &str) -> Result<Box<dyn SerialPort>, serialport::Error> {
+    println!("Initializing serial port: {}", device);
+    let port = serialport::new(device, BAUDRATE)
+        .timeout(Duration::from_secs(1))
+        .data_bits(serialport::DataBits::Eight)
+        .parity(serialport::Parity::None)
+        .stop_bits(serialport::StopBits::One)
+        .flow_control(serialport::FlowControl::None)
+        .open();
 
-    pub fn set_zero_point(&mut self) -> CanFrame {
-        CanFrame::new(self.set_extended_id(6, 0x0000, self.id), &[1]).unwrap()
+    match port {
+        Ok(port) => {
+            println!("Serial port initialized successfully");
+            Ok(port)
+        }
+        Err(e) => {
+            eprintln!("Error opening serial port: {}", e);
+            Err(e)
+        }
     }
+}
 
-    pub fn set_dev_id(&mut self, request_id: u8) -> CanFrame {
-        CanFrame::new(
-            self.set_extended_id(7, (request_id as u16) << 8, self.id),
-            &[1],
-        )
-        .unwrap()
-    }
+fn send_reset(serial_port: &mut dyn SerialPort, id: u8) {
+    let pack = CanPack {
+        ex_id: ExId::new(id, CAN_ID_DEBUG_UI as u16, CanComMode::MotorReset as u8, 0),
+        len: 8,
+        data: [0; 8],
+    };
+    txd_pack(serial_port, &pack);
+}
 
-    pub fn request_param(&mut self, index: u16) -> CanFrame {
-        CanFrame::new(
-            self.set_extended_id(17, 0x0000, self.id),
-            &[index as u8, (index >> 8) as u8],
-        )
-        .unwrap()
-    }
+fn send_start(serial_port: &mut dyn SerialPort, id: u8) {
+    let pack = CanPack {
+        ex_id: ExId::new(id, CAN_ID_DEBUG_UI as u16, CanComMode::MotorIn as u8, 0),
+        len: 8,
+        data: [0; 8],
+    };
+    txd_pack(serial_port, &pack);
+}
 
-    pub fn write_param(&mut self, index: u16, param: u32) -> CanFrame {
-        CanFrame::new(
-            self.set_extended_id(18, 0x0000, self.id),
-            &[
-                index as u8,
-                (index >> 8) as u8,
-                0,
-                0,
-                param as u8,
-                (param >> 8) as u8,
-                (param >> 16) as u8,
-                (param >> 24) as u8,
-            ],
-        )
-        .unwrap()
+pub fn debug_main() {
+    println!("Starting program");
+
+    // Initialize serial port
+    let serial_port = init_serial_port(TTY_PORT);
+    if serial_port.is_err() {
+        eprintln!("Failed to initialize serial port");
+        return;
     }
+    let mut serial_port = serial_port.unwrap();
+
+    // Add a delay after initializing the port
+    std::thread::sleep(Duration::from_millis(500));
+
+    // Initialize the device
+    let device_id: u8 = 1; // Use the default motor address
+    send_start(&mut serial_port, device_id);
+    // send_reset(&mut serial_port, device_id);
+
+    println!("Program finished");
 }
