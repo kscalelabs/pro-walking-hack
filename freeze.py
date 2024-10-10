562b573d9b5d8b2fe1c7f7e1f0fbab2c1dd832fc
Benjamin Bolte <ben@bolte.cc>
2024-10-10 01:15:27 -0700

    motor zeroing

--- freeze.py
+++ freeze.py
@@ -31,6 +31,9 @@ PORT_NAMES = {
     }
 }
 
+STILL_MOVING_TORQUE = 0.1
+TORQUE_LIMIT = 1.0
+
 
 def main() -> None:
     motors = [
@@ -39,6 +42,7 @@ def main() -> None:
                 port_name=port_name,
                 motor_infos=motor_infos,
             ),
+            port_name,
             motor_infos,
         )
         for port_name, motor_infos in PORT_NAMES.items()
@@ -50,40 +54,97 @@ def main() -> None:
         target_velocity = 0.0
 
         # Position plus velocity.
-        kp = 5.0
-        kd = 0.1
+        kp = 1.0
+        kd = 0.3
 
         # Just position.
         # kp = 0.05
         # kd = 0.0
 
         torque = kp * (target_position - cur_position) + kd * (target_velocity - cur_velocity)
-        return torque
+        return min(max(torque, -TORQUE_LIMIT), TORQUE_LIMIT)
 
     # Initialize the motors.
-    for motor, info in motors:
+    for motor, _, _ in motors:
         motor.send_reset()
-        if rs_01 := [i for i, kind in info.items() if kind == "01"]:
-            motor.send_set_zero(rs_01)  # Zero the single-encoder motors.
         motor.send_start()
 
-    try:
-        start_time = time.time()
-        while True:
-            target_position = math.sin(time.time() - start_time) * math.pi
+    def get_motor_positions() -> dict[str, dict[int, float]]:
+        motor_positions: dict[str, dict[int, float]] = {}
+        for motor, port_name, info in motors:
+            feedback = motor.get_latest_feedback()
+            if rs_01 := [i for i, kind in info.items() if kind == "01"]:
+                motor_positions[port_name] = {i: feedback[i].position for i in rs_01}
+        return motor_positions
 
-            for motor, info in motors:
+    def move_until_stopped(k_dp: float) -> dict[str, dict[int, float]]:
+        start_positions = get_motor_positions()
+        still_moving = True
+        start_time = time.time()
+        while still_moving:
+            elapsed_time = time.time() - start_time
+            dp = elapsed_time * k_dp
+            still_moving = elapsed_time < 0.5
+            for motor, port_name, info in motors:
+                if port_name not in start_positions:
+                    continue
+                if rs_01 := [i for i, kind in info.items() if kind == "01"]:
+                    feedback = motor.get_latest_feedback()
+                    torques = {i: get_torque(start_positions[port_name][i] + dp, feedback[i]) for i in rs_01}
+                    motor.send_torque_controls(torques)
+                    still_moving = still_moving or any(abs(torque) < STILL_MOVING_TORQUE for torque in torques.values())
+        return get_motor_positions()
+
+    def zero_motors() -> None:
+        # Slowly move backwards from the starting position until all motors have stopped moving.
+        start_positions = move_until_stopped(k_dp=math.pi)
+        end_positions = move_until_stopped(k_dp=-math.pi)
+
+        # Moves to the middle of the range.
+        mid_positions = {
+            port_name: {
+                i: (start_positions[port_name][i] + end_positions[port_name][i]) / 2 for i in start_positions[port_name]
+            }
+            for port_name in start_positions
+        }
+
+        still_moving = True
+        while still_moving:
+            still_moving = False
+            for motor, port_name, info in motors:
+                if rs_01 := [i for i, kind in info.items() if kind == "01"]:
+                    feedback = motor.get_latest_feedback()
+                    torques = {i: get_torque(mid_positions[port_name][i], feedback[i]) for i in rs_01}
+                    motor.send_torque_controls(torques)
+                    still_moving = still_moving or any(
+                        abs(feedback[i].position - mid_positions[port_name][i]) > 0.1 for i in rs_01
+                    )
+
+        for motor, _, info in motors:
+            if rs_01 := [i for i, kind in info.items() if kind == "01"]:
+                motor.send_torque_controls({i: 0.0 for i in rs_01})
+                motor.send_set_zero(rs_01)
+
+    zero_motors()
+
+    try:
+        # start_time = time.time()
+        while True:
+            # target_position = math.sin(time.time() - start_time) * math.pi
+            target_position = 0.0
+
+            for motor, _, info in motors:
                 feedback = motor.get_latest_feedback()
                 torques = {i: get_torque(target_position, feedback) for i, feedback in feedback.items()}
+                # torques = {i: 0.0 for i in info}
                 motor.send_torque_controls(torques)
 
     except KeyboardInterrupt:
-        print("Resetting motors")
-        for motor, _ in motors:
+        for motor, _, _ in motors:
             motor.send_torque_controls({i: 0.0 for i in info})
 
         time.sleep(0.1)
-        for motor, _ in motors:
+        for motor, _, _ in motors:
             motor.send_reset()
         time.sleep(0.1)
 
