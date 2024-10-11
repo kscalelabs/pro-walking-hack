596e1a80190350cb881e86451ab51178d1bf462d
WT-MM <wesley.maa@gmail.com>
2024-10-11 13:14:43 -0700

    draft

--- run_onnx.py
+++ run_onnx.py
@@ -5,9 +5,33 @@ import time
 import numpy as np
 import onnxruntime as ort
 
+from actuator import RobstrideMotorsSupervisor
+from berryimu import IMU, KalmanFilter
+
+I2C_BUS = 7
 
 def run_onnx_model() -> None:
     session = ort.InferenceSession("position_control.onnx")
+
+    # Initialize the motors.
+    motor_infos = {
+        1: {"04"},
+        2: {"03"},
+        3: {"03"},
+        4: {"04"},
+        5: {"01"},
+    }
+
+    left_leg = RobstrideMotorsSupervisor(port_name="/dev/ttyCH341USB0", motor_infos=motor_infos)
+    right_leg = RobstrideMotorsSupervisor(port_name="/dev/ttyCH341USB1", motor_infos=motor_infos)
+
+    # Initialize IMU.
+    imu = IMU(I2C_BUS)
+    kalman_filter = KalmanFilter(imu)
+
+    command = {"x_vel": 0.0,
+               "y_vel": 0.0,
+               "rot": 0.0}
 
     input_data = {
         "x_vel.1": np.zeros(1).astype(np.float32),
@@ -22,25 +46,53 @@ def run_onnx_model() -> None:
         "buffer.1": np.zeros(574).astype(np.float32),
     }
 
+    def get_angular_velocity() -> np.ndarray:
+        return imu.gyr_rate()
+
+    def get_euler_angles() -> np.ndarray:
+        return kalman_filter.step()
+
     def get_joint_angles() -> np.ndarray:
-        return np.zeros(10).astype(np.float32)
+        angles = np.zeros(10).astype(np.float32)
+        for id in range(5):
+            angles[id] = left_leg.get_latest_feedback(id).position
+            angles[id + 5] = right_leg.get_latest_feedback(id).position
+        return angles
 
     def get_joint_velocities() -> np.ndarray:
-        return np.zeros(10).astype(np.float32)
+        velocities = np.zeros(10).astype(np.float32)
+        for id in range(5):
+            velocities[id] = left_leg.get_latest_feedback(id).velocity
+            velocities[id + 5] = right_leg.get_latest_feedback(id).velocity
+        return velocities
 
-    def send_torques(torques: np.ndarray) -> None:
-        print(torques)
+    def send_positions(positions: np.ndarray) -> None:
+        for motor_id, angle in enumerate(positions[:5]):
+            left_leg.send_position_control(motor_id, angle)
+
+        for motor_id, angle in enumerate(positions[5:]):
+            right_leg.send_position_control(motor_id, angle)
 
     start_time = time.time()
     while True:
         elapsed_time = time.time() - start_time
+
+        input_data["x_vel.1"][0] = command["x_vel"]
+        input_data["y_vel.1"][0] = command["y_vel"]
+        input_data["rot.1"][0] = command["rot"]
+
         input_data["t.1"][0] = elapsed_time
         input_data["dof_pos.1"] = get_joint_angles()
         input_data["dof_vel.1"] = get_joint_velocities()
-        torques, actions, buffer = session.run(None, input_data)
+        input_data["imu_ang_vel.1"] = get_angular_velocity()
+        input_data["imu_euler_xyz.1"] = get_euler_angles()
+
+        positions, actions, buffer = session.run(None, input_data)
+
         input_data["prev_actions.1"] = actions
         input_data["buffer.1"] = buffer
-        send_torques(torques)
+
+        send_positions(positions)
         time.sleep(1 / 50)
 
 
