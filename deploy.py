dab57f68d48728e26d3b9fb3e8d86bafe447f5f3
WT-MM <wesley.maa@gmail.com>
2024-10-30 16:30:05 +0800

    add hexmove logic

--- deploy.py
+++ deploy.py
@@ -1,14 +1,13 @@
 """Use onnxruntime to run a model."""
 
+import threading
 import time
 from dataclasses import dataclass, field
 
+import can
 import numpy as np
 import onnxruntime as ort
 from actuator import RobstrideMotorFeedback, RobstrideMotorsSupervisor
-from berryimu import IMU, KalmanFilter
-
-I2C_BUS = 7
 
 PD_CONSTANTS = {
     1: {
@@ -89,6 +88,49 @@ class RobotData:
     left_arm: ArmData = field(default_factory=ArmData)
     right_arm: ArmData = field(default_factory=ArmData)
 
+def setup_can_interface(channel: str = 'can0', interface: str = 'socketcan') -> can.Bus | None:
+    """Set up the CAN interface."""
+    try:
+        bus = can.interface.Bus(channel=channel, interface=interface)
+        print(f"Connected to CAN interface on {channel}")
+        return bus
+    except Exception as e:
+        print(f"Failed to connect to CAN interface: {e}")
+        return None
+
+def data_receiver(bus: can.Bus, x_angle_data: list[float], y_angle_data: list[float], z_angle_data: list[float], x_velocity_data: list[float], y_velocity_data: list[float], z_velocity_data: list[float]) -> None:
+    """Receive data from the IMU and store it in lists."""
+    try:
+        while True:
+            message = bus.recv(timeout=1.0)  # Wait for a message
+            if message is not None:
+
+                if message.arbitration_id == 0x0B0101B1:
+                    x_angle = int.from_bytes(message.data[0:2], byteorder='little', signed=True) * 0.01
+                    y_angle = int.from_bytes(message.data[2:4], byteorder='little', signed=True) * 0.01
+                    z_angle = int.from_bytes(message.data[4:6], byteorder='little', signed=True) * 0.01
+                    x_angle_data.append(x_angle)
+                    y_angle_data.append(y_angle)
+                    z_angle_data.append(z_angle)
+
+                    x_angle_data.pop(0)
+                    y_angle_data.pop(0)
+                    z_angle_data.pop(0)
+
+                if message.arbitration_id == 0x0B0101B2:
+                    x_velocity = int.from_bytes(message.data[0:2], byteorder='little', signed=True) * 0.01
+                    y_velocity = int.from_bytes(message.data[2:4], byteorder='little', signed=True) * 0.01
+                    z_velocity = int.from_bytes(message.data[4:6], byteorder='little', signed=True) * 0.01
+                    x_velocity_data.append(x_velocity)
+                    y_velocity_data.append(y_velocity)
+                    z_velocity_data.append(z_velocity)
+
+                    x_velocity_data.pop(0)
+                    y_velocity_data.pop(0)
+                    z_velocity_data.pop(0)
+
+    except Exception as e:
+        print(f"Error receiving IMU data: {e}")
 
 def run_onnx_model() -> None:
     session = ort.InferenceSession("standing.onnx")
@@ -102,36 +144,45 @@ def run_onnx_model() -> None:
         5: "01",
     }
 
-    # Initialize IMU.
-    imu = IMU(I2C_BUS)
-    kalman_filter = KalmanFilter(imu)
+    # Initialize CAN-based IMU
+    """
+       sudo ip link set can0 up type can bitrate 500000
+       ip link show can0
+    """
+    bus = setup_can_interface()
+    if bus:
+        buffer_size = 10
+        x_angle_data = [0] * buffer_size
+        y_angle_data = [0] * buffer_size
+        z_angle_data = [0] * buffer_size
+        x_velocity_data = [0] * buffer_size
+        y_velocity_data = [0] * buffer_size
+        z_velocity_data = [0] * buffer_size
+
+        # Start the data receiving thread
+        data_thread = threading.Thread(target=data_receiver, args=(bus, x_angle_data, y_angle_data, z_angle_data, x_velocity_data, y_velocity_data, z_velocity_data, timestamps_angle, timestamps_velocity))
+        data_thread.daemon = True
+        data_thread.start()
 
     # Converting from real to sim
     euler_signs = np.array([1, 1, 1])
 
-    # Calibrate to current euler angles
-    for _ in range(10):
-        time.sleep(0.1)
-        calibration_angle = kalman_filter.step()
+    time.sleep(1)
 
-    # TODO: Check how the imu works in sim
-    euler_offsets = -1 * np.array([calibration_angle.yaw, calibration_angle.pitch, calibration_angle.roll])
+    # IMU offset
+    angular_offset = np.array([x_angle_data[-1], y_angle_data[-1], z_angle_data[-1]])
 
     left_leg = RobstrideMotorsSupervisor(port_name="/dev/ttyCH341USB0", motor_infos=leg_motor_infos,
                                         target_update_rate=10000.0)
 
-    right_leg = RobstrideMotorsSupervisor(port_name="/dev/ttyCH341USB2", motor_infos=leg_motor_infos,
+    right_leg = RobstrideMotorsSupervisor(port_name="/dev/ttyCH341USB1", motor_infos=leg_motor_infos,
                                          target_update_rate=10000.0)
-
 
     robot_data = RobotData()
 
     for i in range(5):
         left_leg.add_motor_to_zero(i + 1)
         right_leg.add_motor_to_zero(i + 1)
-        # left_arm.add_motor_to_zero(i + 1)
-        # if i != 0:
-        #     right_arm.add_motor_to_zero(i + 1)
         time.sleep(0.01)
 
     for motor_id, motor_info in PD_CONSTANTS.items():
@@ -145,7 +196,6 @@ def run_onnx_model() -> None:
 
         left_leg.set_kd(motor_id, motor_info["d"])
         right_leg.set_kd(motor_id, motor_info["d"])
-
 
     input_data = {
         "x_vel.1": np.zeros(1, dtype=np.float32),
@@ -169,13 +219,10 @@ def run_onnx_model() -> None:
     input_data["rot.1"][0] = np.float32(command["rot"])
 
     def get_angular_velocity() -> np.ndarray:
-        vector = imu.gyr_rate()
-        return np.array([vector.x, vector.y, vector.z])
+        return np.array([x_velocity_data[-1], y_velocity_data[-1], z_velocity_data[-1]])
 
     def get_euler_angles() -> np.ndarray:
-        angle = kalman_filter.step()
-
-        return np.deg2rad([angle.yaw, angle.pitch, angle.roll] * euler_signs + euler_offsets)
+        return np.deg2rad([x_angle_data[-1], y_angle_data[-1], z_angle_data[-1]] - angular_offset) * euler_signs
 
     def update_motor_data() -> None:
         left_leg_feedback = left_leg.get_latest_feedback()
@@ -193,7 +240,6 @@ def run_onnx_model() -> None:
             print(f"Right leg feedback length is not 5: {len(right_leg_feedback)}")
 
     def get_joint_angles() -> np.ndarray:
-
         return np.array([
             robot_data.left_leg.hip_y.position,
             robot_data.left_leg.hip_x.position,
@@ -223,12 +269,10 @@ def run_onnx_model() -> None:
 
     def send_positions(positions: np.ndarray) -> None:
         for motor_id, angle in enumerate(positions[:5]):
-            # angle = 0
             left_leg.set_position(motor_id + 1, angle)
             time.sleep(0.01)
 
         for motor_id, angle in enumerate(positions[5:]):
-            # angle = 0
             right_leg.set_position(motor_id + 1, angle)
             time.sleep(0.01)
 
@@ -255,7 +299,6 @@ def run_onnx_model() -> None:
             print(f"Positions out of bounds: {positions}")
             positions = np.clip(positions, -0.1, 0.1) 
             print(f"Dof_pos: {input_data['dof_pos.1']}")
-
 
         send_positions(positions)
 
