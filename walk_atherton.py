bb1326807c27102edde12bdace320363536e00e3
Denys Bezmenov <denys@kscale.dev>
2024-12-11 14:04:27 -0800

    update model

--- walk_atherton.py
+++ walk_atherton.py
@@ -1,23 +1,28 @@
-""" Whoever is reading this, forgive us.
-
-TODO: check assumptions:
-# 1. Check if torque values throguh position contorl are the same - 
-# if not, we should pass straight torque value
-# 2. 
-
-"""
+""" Demo walking script. """
 import time
 import pykos
 import numpy as np
+from collections import OrderedDict
 from kinfer.inference import ONNXModel
-# from imu import HexmoveImuReader
 
+# Define ordered joint mapping signs
+JOINT_MAPPING_SIGNS = OrderedDict([
+    ("L_hip_y", -1),
+    ("L_hip_x", 1),
+    ("L_hip_z", 1),
+    ("L_knee", -1),
+    ("L_ankle", 1),
+    ("R_hip_y", -1),
+    ("R_hip_x", 1),
+    ("R_hip_z", 1),
+    ("R_knee", -1),
+    ("R_ankle", 1)
+])
 
 class RealPPOController:
     def __init__(
             self, 
             model_path: str,
-            joint_mapping_signs: np.ndarray,
             check_default: bool = False,
             kos: pykos.KOS = None
         ):
@@ -36,7 +41,7 @@ class RealPPOController:
             "rot": 0.0,
         }
 
-        self.joint_mapping_signs = joint_mapping_signs
+        self.joint_mapping_signs = np.array(list(JOINT_MAPPING_SIGNS.values()))
 
         # Get model metadata
         metadata = self.kinfer.get_metadata()
@@ -49,12 +54,10 @@ class RealPPOController:
             "default_standing": metadata["default_standing"],
         }
         
-        # Add IMU initialization
-        # self.imu_reader = HexmoveImuReader("can0", 1, 1)
         self.euler_signs = np.array([-1, -1, 1])
 
-        self.left_arm_ids = []# [11, 12, 13, 14, 15]#, 16]
-        self.right_arm_ids = []# [21, 22, 23, 24, 25]#, 26]
+        self.left_arm_ids = []
+        self.right_arm_ids = []
         self.left_leg_ids = [31, 32, 33, 34, 35]
         self.right_leg_ids = [41, 42, 43, 44, 45]
 
@@ -62,12 +65,7 @@ class RealPPOController:
         self.type_three_ids = [limb[id] for limb in [self.left_leg_ids, self.right_leg_ids] for id in [1, 2]]
         self.type_two_ids = [limb[id] for limb in [self.left_leg_ids, self.right_leg_ids] for id in [4]]
 
-        # self.all_ids = self.left_arm_ids + self.right_arm_ids
         self.all_ids = self.left_leg_ids + self.right_leg_ids
-
-        commands = [{"actuator_id": id, "position": 0} for id in self.all_ids]
-
-        self.kos.actuator.command_actuators(commands)
 
         # Configure all motors
         for id in self.type_four_ids:
@@ -79,16 +77,6 @@ class RealPPOController:
         for id in self.type_two_ids:
             self.kos.actuator.configure_actuator(actuator_id=id, kp=17, kd=5, max_torque=10, torque_enabled=True)
 
-        # # Calculate initial IMU offset as running average over 5 seconds
-        # num_samples = 50  # 10 Hz for 5 seconds
-        # angles = []
-        # print("Calculating IMU offset...")
-        # for _ in range(num_samples):
-        #     imu_data = self.imu_reader.get_data()
-        #     angles.append([imu_data.x_angle, imu_data.y_angle, imu_data.z_angle])
-        #     time.sleep(0.1)
-
-        # self.angular_offset = np.mean(angles, axis=0)
         self.angular_offset = np.array([0, 0, 0])
         print(f"IMU offset calculated: {self.angular_offset}")
 
@@ -116,43 +104,15 @@ class RealPPOController:
         self.actions = np.zeros(self.model_info["num_actions"], dtype=np.float32)
         self.buffer = np.zeros(self.model_info["num_observations"], dtype=np.float32)
 
-        breakpoint()
         if check_default:
             self.set_default_position()
-            time.sleep(3)
-        exit()
+            time.sleep(2)
 
     def update_robot_state(self):
         """Update input data from robot sensors"""        
-        # Get IMU data
-        # imu_data = self.imu_reader.get_data()
-
-        # # # Calculate IMU angular velocity
-        # imu_ang_vel = np.deg2rad([
-        #     imu_data.x_velocity, 
-        #     imu_data.y_velocity, 
-        #     imu_data.z_velocity
-        # ]) * self.euler_signs
-        
-        # # Calculate IMU orientation (euler angles)
-        # angles = np.deg2rad([
-        #     imu_data.x_angle,
-        #     imu_data.y_angle, 
-        #     imu_data.z_angle
-        # ] - self.angular_offset) * self.euler_signs
-        
-        # # Normalize angles to [-pi, pi]
-        # angles[angles > np.pi] -= 2 * np.pi
-        # angles[angles < -np.pi] += 2 * np.pi
-
-        # print(f"IMU ang vel: {imu_ang_vel}")
-
         # Debugging
         imu_ang_vel = np.asarray([0, 0, 0])
         angles = np.asarray([0, 0, 0])
-
-        # Clip imu_ang_vel
-        # imu_ang_vel = np.clip(imu_ang_vel, -0.2, 0.2)
         
         print(f"Angles: {angles}")
 
@@ -179,11 +139,8 @@ class RealPPOController:
             np.array([self.motor_feedback_dict[id].velocity for id in self.right_leg_ids])
         ])
 
-        # TODO: Align
         joint_positions = np.deg2rad(joint_positions)
-
         joint_velocities = np.deg2rad(joint_velocities)
-
         joint_positions -= self.offsets
 
         joint_positions = self.joint_mapping_signs * joint_positions
@@ -256,24 +213,10 @@ class RealPPOController:
 
 if __name__ == "__main__":
     kos = pykos.KOS()
-    joint_mapping_signs = [
-        ("L_hip_y", 1),
-        ("L_hip_x", 1), 
-        ("L_hip_z", 1),
-        ("L_knee", -1),
-        ("L_ankle", 1),
-        ("R_hip_y", -1),
-        ("R_hip_x", 1),
-        ("R_hip_z", 1), 
-        ("R_knee", -1),
-        ("R_ankle", 1)
-    ]
-    motor_signs = np.asarray([1, 1, 1, -1, 1,  -1, 1, 1, -1, 1])
-
-    model = "gpr_walking_pr126.kinfer"
+    
+    model = "gpr_walking_126.kinfer"
     controller = RealPPOController(
         model_path=model,
-        joint_mapping_signs=motor_signs,
         check_default=True,
         kos=kos,
     )
@@ -289,7 +232,6 @@ if __name__ == "__main__":
     kos.process_manager.start_kclip(f"walking_{timestamp}")
     time.sleep(1)
     frequency = 1/100. # 100Hz
-    frequency = 1/10.
     start_time = time.time()
 
     try:
