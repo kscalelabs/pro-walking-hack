e42b6183c485263c8e31e66d77066135b3dfe3b6
Denys Bezmenov <denys@kscale.dev>
2024-12-11 00:07:54 -0800

    adjust to the current state

--- demo.py
+++ demo.py
@@ -34,26 +34,26 @@ def set_up(kos):
     type_two_ids = [limb[id] for limb in [left_leg_ids, right_leg_ids] for id in [4]]
 
     get_state(kos)
-    breakpoint()
 
     # Configure all motors
     for id in type_four_ids:
         print(f"Configuring actuator {id}")
         kos.actuator.configure_actuator(actuator_id=id, kp=120, kd=10, max_torque=20, torque_enabled=True)
-        time.sleep(0.1)
+        # time.sleep(0.1)
 
     for id in type_three_ids:
         print(f"Configuring actuator {id}")
         kos.actuator.configure_actuator(actuator_id=id, kp=60, kd=5, max_torque=15, torque_enabled=True)
-        time.sleep(0.1)
+        # time.sleep(0.1)
     for id in type_two_ids:
         print(f"Configuring actuator {id}")
         kos.actuator.configure_actuator(actuator_id=id, kp=34, kd=5, max_torque=10, torque_enabled=True)
-        time.sleep(0.1)
-    breakpoint()
+        # time.sleep(0.1)
+
     for id in arm_ids:
         print(f"Configuring actuator {id}")
         kos.actuator.configure_actuator(actuator_id=id, kp=10, kd=3, max_torque=5, torque_enabled=True)
+        # time.sleep(0.1)
 
     get_state(kos)
 
@@ -72,6 +72,7 @@ def get_to_position(kos, motor_id, current_position, target_position, time_perio
     """
     interpolation_factor = np.linspace(current_position, target_position, time_period * frequency)
 
+    print(f"current position: {current_position}, target position: {target_position}")
     for pos in interpolation_factor:
         cmd = [{"actuator_id": motor_id, "position": pos}]
         kos.actuator.command_actuators(commands=cmd)
@@ -92,6 +93,7 @@ def get_to_positions(kos, motor_ids, current_positions, target_positions, time_p
     """
     interpolation_factor = np.linspace(current_positions, target_positions, time_period * frequency)
 
+    print(f"current positions: {current_positions}, target positions: {target_positions}")
     for pos in interpolation_factor:
         cmd = [{"actuator_id": motor_id, "position": pos} for motor_id in motor_ids]
         kos.actuator.command_actuators(commands=cmd)
@@ -104,21 +106,22 @@ def wave(kos):
     yaw_id = 24
     gripper_id = 26
 
-    breakpoint()
     get_to_position(kos, roll_id, 0, -10, 1, 50)
     # roll elbow
     get_to_position(kos, roll_id, 0, -60, 1, 50)
     # yaw elbow
-    get_to_position(kos, yaw_id, 0, 90, 1, 50)
-
+    get_to_position(kos, yaw_id, 0, 80, 2, 50)
+    # get_to_position(kos, yaw_id, 77, 0, 2, 50)
     time_period = 20
     start_time = time.time()
     while time.time() - start_time < time_period:
-        get_to_position(kos, yaw_id, 90, 180, 1, 50)
-        get_to_position(kos, gripper_id, 0, 100, 0.5, 50)
-
-        get_to_position(kos, yaw_id, 180, 90, 1, 50)
-        get_to_position(kos, gripper_id, 100, 0, 0.5, 50)
+        yaw_pos = get_state(kos)[3].position
+        get_to_position(kos, yaw_id, yaw_pos, 128, 2, 50)
+       
+        # get_to_position(kos, gripper_id, 0, 100, 0.5, 50)
+        yaw_pos = get_state(kos)[3].position
+        get_to_position(kos, yaw_id, yaw_pos, 80, 2, 50)
+        # get_to_position(kos, gripper_id, 100, 0, 0.5, 50)
 
 
 if __name__ == "__main__":
